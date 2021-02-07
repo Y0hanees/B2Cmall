b068b104a79d72db4d3b9e5470fcfa2ca94726d9
Yohanes <Y0hanes@outlook.com>
2021-02-07 18:11:52 +0800

    模块创建

---
+++ mail-conpon/src/main/java/com/mail/mailconpon/MailConponApplication.java
@@ -0,0 +1,13 @@
+package com.mail.mailconpon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailConponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailConponApplication.class, args);
+    }
+
+}
---
+++ mail-conpon/src/test/java/com/mail/mailconpon/MailConponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mail.mailconpon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailConponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
